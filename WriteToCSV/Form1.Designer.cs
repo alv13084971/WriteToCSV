b02021d7a083ea4c1127eca388d1001af42fc176
alv13084971 <33397259+alv13084971@users.noreply.github.com>
2017-12-17 13:06:39 +0000

    Add project files.

---
+++ WriteToCSV/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WriteToCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.Surname = new System.Windows.Forms.Label();
+            this.Country = new System.Windows.Forms.Label();
+            this.City = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(282, 122);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(364, 122);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(119, 28);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 3;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(226, 28);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 20);
+            this.txtCountry.TabIndex = 4;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(333, 28);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 5;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 6;
+            this.lblName.Text = "Name";
+            // 
+            // Surname
+            // 
+            this.Surname.AutoSize = true;
+            this.Surname.Location = new System.Drawing.Point(119, 9);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(49, 13);
+            this.Surname.TabIndex = 7;
+            this.Surname.Text = "Surname";
+            // 
+            // Country
+            // 
+            this.Country.AutoSize = true;
+            this.Country.Location = new System.Drawing.Point(226, 8);
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(43, 13);
+            this.Country.TabIndex = 8;
+            this.Country.Text = "Country";
+            // 
+            // City
+            // 
+            this.City.AutoSize = true;
+            this.City.Location = new System.Drawing.Point(333, 8);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(24, 13);
+            this.City.TabIndex = 9;
+            this.City.Text = "City";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 157);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.Country);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Write to csv";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label Surname;
+        private System.Windows.Forms.Label Country;
+        private System.Windows.Forms.Label City;
+    }
+}
+
---
+++ WriteToCSV/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WriteToCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (FileStream fs = new FileStream("file.csv", FileMode.Append, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine("{0},{1},{2},{3}", txtName.Text, txtSurname.Text, txtCountry.Text, txtCity.Text);
+            }
+
+            txtName.Text = "";
+            txtSurname.Text = "";
+            txtCountry.Text = "";
+            txtCity.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
